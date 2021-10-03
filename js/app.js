5392862544917432c41e86c3d9e906ee51a780b0
unknown <shizusenpai97@gmail.com>
2021-10-03 17:02:55 -0300

    terminada la seccion equipo

--- js/app.js
+++ js/app.js
@@ -16,3 +16,21 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+var swiper3 = new Swiper(".swiper-equipo", {
+  spaceBetween: 30,
+  loop: true,
+  breakpoints: {
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
